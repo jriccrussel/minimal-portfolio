4c3a84a2888c1591a08f50a8fb8a5004d1c80a92
jriccrussel <jriccrussel@gmail.com>
2022-02-15 04:21:02 +0800

    gsap scrolltrigger && smooth-scrollbar - success && ongoing

--- src/sections/index.js
+++ src/sections/index.js
@@ -89,6 +89,74 @@ const Index1 = () => {
     bodyScrollBar.addListener(ScrollTrigger.update)
 
     ScrollTrigger.defaults({ scroller: scroller })
+
+    // let heroTxtOne = document.querySelector('.t1')
+    // heroTxtOne.innerHTML = heroTxtOne.textContent.replace(/\S/g, "<span class='letter'>$&</span>")
+
+    // let heroTxtTwo = document.querySelector('.t2')
+    // heroTxtTwo.innerHTML = heroTxtTwo.textContent.replace(/\S/g, "<span class='letter'>$&</span>")
+
+    // let heroTxtTri = document.querySelector('.t3')
+    // heroTxtTri.innerHTML = heroTxtTri.textContent.replace(/\S/g, "<span class='letter'>$&</span>")
+
+    // let heroTxtFour = document.querySelector('.t4')
+    // heroTxtFour.innerHTML = heroTxtFour.textContent.replace(/\S/g, "<span class='letter'>$&</span>")
+
+    // gsap.from(heroTxtOne, {
+    //     duration: 0.5,
+    //     opacity: 0,
+    //     autoAlpha: 0,
+    //     yPercent: 0,
+    //     ease: "power4.inOut",
+    //     stagger: 0.5,
+    //     scrollTrigger: {
+    //         trigger: '.header',
+    //         start: 'top center-=20%',
+    //         scrub: true,
+    //     }
+    // }, 1)
+
+    // gsap.from(heroTxtTwo, {
+    //     duration: 0.7,
+    //     opacity: 0,
+    //     autoAlpha: 0,
+    //     yPercent: 0,
+    //     ease: "power4.inOut",
+    //     stagger: 0.7,
+    //     scrollTrigger: {
+    //         trigger: '.header',
+    //         start: 'top',
+    //         scrub: true,
+    //     }
+    // }, 1.2)
+
+    // gsap.from(heroTxtTri, {
+    //     duration: 0.9,
+    //     opacity: 0,
+    //     autoAlpha: 0,
+    //     yPercent: 0,
+    //     ease: "power4.inOut",
+    //     stagger: 0.9,
+    //     scrollTrigger: {
+    //         trigger: '.header',
+    //         start: 'top',
+    //         scrub: true,
+    //     }
+    // }, 1.3)
+
+    // gsap.from(heroTxtFour, {
+    //     duration: 1.1,
+    //     opacity: 0,
+    //     autoAlpha: 0,
+    //     yPercent: 0,
+    //     ease: "power4.inOut",
+    //     stagger: 1.1,
+    //     scrollTrigger: {
+    //         trigger: '.header',
+    //         start: 'top',
+    //         scrub: true,
+    //     }
+    // }, 1.5)
   }, [])
 
   return (
