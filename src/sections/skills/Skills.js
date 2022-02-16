8be7a07011c235e83cdbeda399df1f64ad1db2de
jriccrussel <jriccrussel@gmail.com>
2022-02-16 13:59:19 +0800

    skills component changes

--- src/sections/skills/Skills.js
+++ src/sections/skills/Skills.js
@@ -8,7 +8,7 @@ const Skills = () => {
             <div className="skills-container">
                 <p>
                     <br />
-                    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;React,&nbsp;NextJs,&nbsp;GSAP,&nbsp;Styled Components,&nbsp;Tailwindcss,&nbsp;Shopify,&nbsp;Wordpress,&nbsp;SCSS,&nbsp;HTML,&nbsp;CSS,&nbsp;Adobe XD,&nbsp;Photoshop,&nbsp;Illustrator,&nbsp;Figma
+                    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;React,&nbsp;NextJs,&nbsp;GSAP,&nbsp;Styled Components,&nbsp;Tailwindcss,&nbsp;Shopify,&nbsp;Wordpress,&nbsp;SCSS,&nbsp;HTML,&nbsp;CSS,&nbsp;Photoshop,&nbsp;Illustrator,&nbsp;Figma,&nbsp;Adobe XD
                     <br />
                 </p>
             </div>
